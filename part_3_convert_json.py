13948fcc049a4084dcff0d1d7c62b0150b38c214
emily-mcdonald <43346892+emily-mcdonald@users.noreply.github.com>
2019-02-26 18:02:20 -0500

    Phase 3 final

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -14,36 +14,34 @@ import cc_data
 def convert_json_to_dat(json_file):
 
     game_data = cc_data.CCDataFile()
-    game_data.game = json_file["levels"]
-
-    for level in json_file:
+    for level in json_file["levels"]:
         game_level = cc_data.CCLevel()
-        game_level.level_number = level[0]
-        game_level.time = level[0]
-        game_level.num_chips = level[0]
-        game_level.upper_layer = level[0]
-        optional_fields = level[4]
+        game_level.level_number = level["level number"]
+        game_level.time = level["time"]
+        game_level.num_chips = level["chip number"]
+        game_level.upper_layer = level["upper layer"]
+        optional_fields_json = level["optional fields"]
 
-        for something in optional_fields:
-            if something[0] == "title":
+        for something in optional_fields_json:
+            if something["id"] == "title":
                 title = cc_data.CCMapTitleField(something["level title"])
                 game_level.add_field(title)
-            elif something[0] == "password":
+            elif something["id"] == "password":
                 password = cc_data.CCEncodedPasswordField(something["encoded password"])
                 game_level.add_field(password)
-            elif something[0] == "hint":
+            elif something["id"] == "hint":
                 hint = cc_data.CCMapHintField(something["hint text"])
                 game_level.add_field(hint)
-            elif something[0] == "monsters":
+            elif something["id"] == "monsters":
                 coordinates = []
                 for coordinate in something["monsters"]:
-                    x = coordinates[0]
-                    y = coordinates[1]
+                    x = coordinate[0]
+                    y = coordinate[1]
                     coord = cc_data.CCCoordinate(x,y)
                     coordinates.append(coord)
 
 
-    game_data.add_level(game_level)
+        game_data.add_level(game_level)
     return game_data
 
 
